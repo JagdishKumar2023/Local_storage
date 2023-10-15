c513d7adc6bd5122f553bdb471534151ef3847fd
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2023-10-15 12:47:13 +0530

    New Changes

---
+++ Cookies/app.js
@@ -0,0 +1,10 @@
+// document.cookie = "Item=milk";
+// document.cookie = "location=kakatea";
+
+// document.cookies = "loc=india";
+
+// document.cookie = "item=bread,Eggs; expires=Sat, 20 Mar 2023 12:00:00 UTC";
+
+// let cookieData = document.cookie;
+
+// console.log(cookieData);
